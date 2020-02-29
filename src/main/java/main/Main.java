0d5f1adc53169dd392aea5dd80e640d4bbcf5d2d
Kamil Olszewski <kamil.j.olszewski@gmail.com>
2020-02-29 11:14:14 +0100

    builder implementation

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        BarChart barChart = new BarChart("Task types report",new ArrayList());
+        BarChart barChart = new BarChart("Task types report", new ArrayList());
+
+        BarChart barChartMadeWIthBuilder = BarChart.builder()
+                .withTitle("test")
+                .withFontSize(5)
+                .build();
     }
 }
